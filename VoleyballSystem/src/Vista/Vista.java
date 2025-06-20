952783e67af2f55851e06960bf188a22970cebac
martina <Usuario@DESKTOP-K01J02V>
2025-06-20 10:20:16 -0300

    guardar cambios antes del merge

---
+++ VoleyballSystem/src/Vista/Vista.java
@@ -0,0 +1,17 @@
+package Vista;
+
+import java.util.Scanner;
+
+public class Vista {
+
+    private Scanner sc = new Scanner(System.in);
+    public void mostrarMenuPrincipal() {
+        System.out.println("\n... Menú Campeonato Vóley ...");
+        System.out.println("1. Registrar Campeonato");
+        System.out.println("2. Listar Campeonatos");
+        System.out.println("3. Seleccionar Campeonato");
+        System.out.println("0. Salir");
+    }
+
+
+}
--- VoleyballSystem/src/modelo/Cancha.java
+++ VoleyballSystem/src/modelo/Cancha.java
@@ -1,36 +1,11 @@
 package modelo;
 
 public class Cancha {
-    private String nombre;
+    private int numero;
     private String ubicacion;
 
-    public Cancha(String nombre, String ubicacion) {
-        this.nombre = nombre;
-        this.ubicacion = ubicacion;
+    public Cancha(int numero) {
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getUbicacion() {
-        return ubicacion;
-    }
-
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    @Override
-    public String toString() {
-        return "Cancha{" +
-                "nombre='" + nombre + '\'' +
-                ", ubicacion='" + ubicacion + '\'' +
-                '}';
-    }
 }
 
